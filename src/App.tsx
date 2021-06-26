9b4b27b7414c214109e4bedcbe57cc7fb0c95715
momosetkn <hyakkun@gmail.com>
2021-06-27 00:19:47 +0900

    For research

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,11 @@ type State = {
   motionY: number,
   motionZ: number,
   x: number,
+  speedX: number,
+  speedY: number,
+  speedZ: number,
+  distance: number,
+  measurementTime: number,
 }
 
 function App() {
@@ -21,6 +26,11 @@ function App() {
     motionY: 0,
     motionZ: 0,
     x: 0,
+    speedX: 0,
+    speedY: 0,
+    speedZ: 0,
+    distance: 0,
+    measurementTime: new Date().getTime(),
   });
 
   useEffect(() => {
@@ -37,6 +47,25 @@ function App() {
     window.addEventListener("devicemotion", (dat) => {
       if (!dat.acceleration) return;
       const {x, y, z} = dat.acceleration;
+
+      const { measurementTime, speedX, speedY, speedZ } = state;
+      const nowMeasurementTime = new Date().getTime();
+
+      const diffTime = nowMeasurementTime - measurementTime;
+
+      const extracted = ({acceleration, speed}:{acceleration: number, speed: number}) => {
+        const nowSpeed = (acceleration) * diffTime;
+        const nowDistance =  (nowSpeed + speed) * diffTime
+        return {nowDistance, nowSpeed};
+      }
+
+      const xx = extracted({acceleration: x || 0, speed: speedX});
+      const yy = extracted({acceleration: y || 0, speed: speedY});
+      const zz = extracted({acceleration: z || 0, speed: speedZ});
+      // const calcDistance = (a: number, b: number, c: number) => Math.sqrt(Math.sqrt(a**2 + b**2)**2 + c**2);
+      // distance(distanceX, distanceY, distanceZ);
+      // x*(diffTime**2)
+      const distance = Math.sqrt(Math.sqrt(xx.nowDistance**2 + yy.nowDistance**2)**2 + zz.nowDistance**2);
       update(prev => (
         {
           ...prev,
@@ -44,6 +73,11 @@ function App() {
           motionY: y || 0,
           motionZ: z || 0,
           x: prev.x+1,
+          speedX: xx.nowSpeed,
+          speedY: yy.nowSpeed,
+          speedZ: zz.nowSpeed,
+          distance: distance,
+          measurementTime: nowMeasurementTime,
         }));
     });
   }, []);
@@ -82,6 +116,22 @@ function App() {
         <tr>
           <td>x</td>
           <td>{state.x}</td>
+        </tr>
+        <tr>
+          <td>speedX</td>
+          <td>{state.speedX}</td>
+        </tr>
+        <tr>
+          <td>speedY</td>
+          <td>{state.speedY}</td>
+        </tr>
+        <tr>
+          <td>speedZ</td>
+          <td>{state.speedZ}</td>
+        </tr>
+        <tr>
+          <td>distance</td>
+          <td>{state.distance}</td>
         </tr>
       </table>
     </StyledMain>
